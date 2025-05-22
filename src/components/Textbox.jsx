8f070b0af76654a0c29530189dd14b935593a963
Afonso <Afonydasilva@gmail.com>
2025-05-22 16:19:37 +0100

    Endireitando a responsividade do input

--- src/components/Textbox.jsx
+++ src/components/Textbox.jsx
@@ -14,7 +14,7 @@ function Textbox(props){
     return(
     <div>
       
-    <input ref={inputRef} value={props.senha} className="mx-5 bg-white w-92 md:w-160 lg:w-200 rounded text-center outline-slate-400 py-1 px-4 text-black text-xs md:text-lg lg:text-2xl" />
+    <input ref={inputRef} value={props.senha} className="mx-5 bg-white w-50 md:w-160 lg:w-200 rounded text-center outline-slate-400 py-1 px-4 text-black text-xs md:text-lg lg:text-2xl" />
     <button onClick={copiar} className="cursor-pointer rounded"><Copy size={30} color="green" className="-my-3 lg:-my-6 -mx-3 text-green-500 w-5 md:w-7 lg:w-10"/></button>
     </div>
     )
